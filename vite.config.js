26ae67aeb3d398218116b760ae6497cb44a769fb
woszma <wsmforcode@gmail.com>
2025-06-08 20:40:27 +0800

    Fix JSX handling and MIME type issues

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+      jsxImportSource: 'react',
+      babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]
+        ]
+      }
+    })
+  ],
   base: './',
   build: {
     outDir: 'dist',
@@ -13,6 +23,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html'
+      },
+      output: {
+        format: 'es',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     }
   }
